2bdc936b7539b060c3863833f1167eaff4b00408
Ajit Dev <ajitk23192@gmail.com>
2026-04-30 21:39:20 +0530

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        //// Linear Search O(n)
+    //    int n = nums.size();
+    //    for(int i = 0; i<n ; i++){
+    //         if(nums[i] == target){
+    //             return i;
+    //         }
+    //    } 
+    //    return -1;
+
+
+
+    //// this is Binary Search O(log n)
+    int st =0 , end = nums.size();
+    while(st<=end){
+        int mid = st + (end-st)/2;
+        if(nums[mid] == target){
+            return mid;
+        }else if(nums[mid] > target){
+            st = mid +1;
+        }else{
+            end = mid-1;
+        }
+
+    }
+    return -1;
+    }
+};
