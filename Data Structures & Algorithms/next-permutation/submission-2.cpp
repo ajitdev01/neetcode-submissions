782798419effc1be06be087b5843826d1c12fc00
Ajit Dev <ajitk23192@gmail.com>
2026-05-03 20:20:12 +0530

    Add: next-permutation - submission-2

---
+++ Data Structures & Algorithms/next-permutation/submission-2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &A)
+    {
+
+        //// using STL 😅😅😅 not use ShortCuts
+        // next_permutation(A.begin(), A.end());
+
+
+
+        // find the Pivit
+        int pivot = -1, n = A.size();
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (A[i] < A[i + 1])
+            {
+                pivot = i;
+                break;
+            }
+        }
+        if (pivot == -1)
+        {
+            reverse(A.begin(), A.end());
+            return;
+        }
+        // 2nd Step : next larger number
+        for (int i = n - 1; i > pivot; i--)
+        {
+            if (A[i] > A[pivot])
+            {
+                swap(A[i], A[pivot]);
+                break;
+            }
+        }
+        //// 3rd Step : Revers (piv+1 to n-1)
+        // reverse(A.begin() + pivot + 1, A.end());
+        int i = pivot + 1, j = n - 1;
+        while (i <= j)
+        {
+            swap(A[i], A[j]);
+            i++;
+            j--;
+        }
+    }
+};
