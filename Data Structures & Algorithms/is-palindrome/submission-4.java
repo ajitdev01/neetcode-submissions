34b328b014c218437a62a1f16deee6889dc4c06d
Ajit Dev <ajitk23192@gmail.com>
2026-05-12 09:14:37 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    public boolean isAlphaNum(char ch) {
+
+        if (
+            (ch >= '0' && ch <= '9') ||
+            (Character.toLowerCase(ch) >= 'a' &&
+             Character.toLowerCase(ch) <= 'z')
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean isPalindrome(String s) {
+
+        int st = 0;
+        int end = s.length() - 1;
+
+        while (st <end) {
+
+            if (!isAlphaNum(s.charAt(st))) {
+                st++;
+                continue;
+            }
+
+            if (!isAlphaNum(s.charAt(end))) {
+                end--;
+                continue;
+            }
+
+            if (
+                Character.toLowerCase(s.charAt(st)) !=
+                Character.toLowerCase(s.charAt(end))
+            ) {
+                return false;
+            }
+
+            st++;
+            end--;
+        }
+
+        return true;
+    }
+}
