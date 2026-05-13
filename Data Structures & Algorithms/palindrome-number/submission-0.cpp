198fe7b8af9e85624b9705ad63b521eb10f85d96
Ajit Dev <ajitk23192@gmail.com>
2026-05-13 10:11:07 -0400

    Add: palindrome-number - submission-0

---
+++ Data Structures & Algorithms/palindrome-number/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    int reverse(int n)
+    {
+        int reverseNum = 0;
+        while (n != 0)
+        {
+            int dig = n % 10;
+
+            if (reverseNum > INT_MAX / 10 || reverseNum < INT_MIN / 10)
+            {
+                return 0;
+            }
+
+            reverseNum = reverseNum * 10 + dig;
+
+            n = n / 10;
+        }
+        return reverseNum;
+    }
+    bool isPalindrome(int n)
+    {
+        if(n<0) return false;
+
+        int reverseNumber = reverse(n);
+
+        if(n==reverseNumber ) return true;
+        return false;
+
+        
+        
+    }
+};
