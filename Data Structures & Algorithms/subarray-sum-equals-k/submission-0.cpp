c15219491cf012ef51fb9fd9353b792075b1a3db
Ajit Dev <ajitk23192@gmail.com>
2026-05-19 04:33:31 -0400

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.cpp
@@ -0,0 +1,22 @@
+///// Burte Force Approch
+class Solution
+{
+public:
+    int subarraySum(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        int count = 0;
+        for (int i = 0; i < n; i++) // starting Point
+        {
+            int sum = 0;
+            for (int j = i; j < n; j++)
+            { // nding
+                sum += nums[j];
+                if (sum == k)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+};
