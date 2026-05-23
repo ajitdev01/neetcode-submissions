75db7298b6ab45914b6c95d0ca8f24517b879bfb
Ajit Dev <ajitk23192@gmail.com>
2026-05-23 19:22:55 +0530

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.ts
@@ -0,0 +1,34 @@
+class Solution {
+
+    getPermuta(
+        arr: number[],
+        idx: number,
+        ans: number[][]
+    ): void {
+
+        let n = arr.length;
+
+        if (idx === n) {
+            ans.push([...arr]);
+            return;
+        }
+
+        for (let i = idx; i < n; i++) {
+
+            [arr[idx], arr[i]] = [arr[i], arr[idx]];
+
+            this.getPermuta(arr, idx + 1, ans);
+
+            [arr[idx], arr[i]] = [arr[i], arr[idx]]; // backtracking
+        }
+    }
+
+    permute(nums: number[]): number[][] {
+
+        let ans: number[][] = [];
+
+        this.getPermuta(nums, 0, ans);
+
+        return ans;
+    }
+}
