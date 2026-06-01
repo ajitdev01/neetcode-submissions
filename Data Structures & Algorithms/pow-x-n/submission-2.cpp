61bb625f9dbd3a7f0b8007dbc77ea56f32f92c46
Ajit Dev <ajitk23192@gmail.com>
2026-06-01 12:40:07 +0530

    Add: pow-x-n - submission-2

---
+++ Data Structures & Algorithms/pow-x-n/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        /// Binary Exponentiations
+
+
+
+
+        /// Short Answer using STL 
+       return pow(x,n);
+
+    }
+};
