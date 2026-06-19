30635fb6269a63bd9a10d0cc839bcd8bd6be11f1
Ajit Dev <ajitk23192@gmail.com>
2026-06-19 10:07:57 +0530

    Add: split-array-largest-sum - submission-0

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-0.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+     ///// Same Like Book Allocation ...
+    bool Isvalid(vector<int> &nums , int n , int k , int mid){
+        int curr = 1;
+        int life = 0;
+        for(int i = 0 ; i<n ; i++ ){
+            if(nums[i]> mid){
+                return false;
+            }else if(life + nums[i] <= mid){
+                life += nums[i];
+            }else{
+                curr++;
+                life = nums[i];
+            }
+        }
+        return curr <= k;
+    }
+
+
+    int splitArray(vector<int>& nums, int k) {
+        int  n = nums.size();
+
+        if(k>n){
+            return -1;
+        }
+        int Sum = 0;
+        for(int val : nums){
+            Sum += val;
+        }
+
+        int st = 0 , end = Sum, ans = -1;
+
+        while(st<=end){
+            int mid = st + (end - st)/2;
+            if(Isvalid(nums,n ,k , mid)){
+                ans = mid;
+                end = mid-1;
+            }else{
+                st = mid+1;
+            }
+        }
+
+        return ans;
+    }
+};
