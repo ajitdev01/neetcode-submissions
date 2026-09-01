3e577bdfba8d05d840caae9a89765396cbe72d9b
AJIT DEV <ajitk23192@gmail.com>
2026-09-01 12:09:13 +0530

    Add: pow-x-n - submission-4

---
+++ Data Structures & Algorithms/pow-x-n/submission-4.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        /// Using STL BUT not use
+        // return pow(x,n);   
+
+
+        /// Logic
+        if(n == 0 ) return 1.0;
+        if(x == 0 ) return 0.0;
+        if(x == -1 && n % 2 == 0 ) return 1.0;
+        if(x == -1 && n % 2  != 0) return -1.0;
+
+        long binForm = n;
+        if(n < 0){
+          x = 1/x; 
+           binForm = - binForm;
+        }
+
+        double ans = 1;
+        while(binForm > 0 ){
+            if(binForm  % 2 == 1){
+                ans *= x;
+            }
+            x *= x;
+            binForm /= 2;
+        }
+
+        return ans;
+        
+
+
+
+    }
+
+};
