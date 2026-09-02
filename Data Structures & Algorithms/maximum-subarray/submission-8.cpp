2756eaa90951f678184537f0defb0a15a38f44c9
AJIT DEV <ajitk23192@gmail.com>
2026-09-02 21:55:00 +0530

    Add: maximum-subarray - submission-8

---
+++ Data Structures & Algorithms/maximum-subarray/submission-8.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &A)
+    {
+
+        // //// Kadane's Algorithm
+        // int currSum = 0 , maxSum = INT_MIN;
+        // for(int i = 0; i<A.size(); i++){
+        //     currSum += A[i];
+        //     maxSum = max(maxSum, currSum);
+        //     if(currSum < 0){
+        //         currSum = 0;
+        //     }
+        // }
+        // return maxSum;
+
+        //     //// Kadane's Algorithm
+        //     int currSum = 0, maxSum = INT_MIN;
+        //        for(int i = 0; i < A.size(); i++) {
+        //        currSum += A[i]; // Current subarray ka sum
+        //   	maxSum = max(currSum, maxSum); // Maximum update
+        //     if(currSum < 0) { // Agar loss ho raha hai
+        // 	currSum = 0; // Naya subarray start karo
+        //  	}
+        //      }
+        //    return maxSum;
+
+        
+
+        //// Kadane's Algorithm
+        int currSum  = 0  , maxSum = INT_MIN;
+        for(int val : A){
+           currSum += val;
+           maxSum = max(maxSum , currSum);
+           if(currSum < 0){
+              currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
