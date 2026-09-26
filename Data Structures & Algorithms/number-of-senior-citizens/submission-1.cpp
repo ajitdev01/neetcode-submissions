75e335ade1f8ec3367b44441aeffe79bc27a895c
AJIT DEV <ajitk23192@gmail.com>
2026-09-26 15:08:18 +0530

    Add: number-of-senior-citizens - submission-1

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+
+        for (string passenger : details) {
+            int age = (passenger[11] - '0') * 10 
+                    + (passenger[12] - '0');
+
+            if (age > 60) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
